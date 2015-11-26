3c029592e5189449670e150cabec4e49b97819c3
ClimbsRocks <climbsbytes@gmail.com>
2015-11-25 20:50:41 -0800

    removes old comments and adds new ones

--- appendPredictionsToValidationData.py
+++ appendPredictionsToValidationData.py
@@ -25,29 +25,20 @@ def load_sparse_csr(filename):
 predictions = []
 with open(predictionsFile, 'rU') as predictionsFile:
     inputRows = csv.reader(predictionsFile)
-    # one option would be to create a new sparse matrix that is the size of our inputRows matrix
     for row in inputRows:
         predictions.append(row)
 
-# predictions = np.ndarray(predictions)
-# predictions = predictions.astype(float)
 
+# scipy.sparse matrices work on np.arrays, not python lists
+# they also do not deal well with mixed data types
+# TODO: this is potentially fragile as we expand to more and more problem types. 
+    # the predictions files will likely always be floats, but the validation data oftentimes won't be
 predictions = np.array(predictions, dtype=float)
 predictions = csr_matrix(predictions)
 
 validationData = load_sparse_csr(validationData)
 
-# put our predictions data into sparse format, so we can stack it with our validationData
-# predictionsSparse = csr_matrix(np.array(predictions))
-
-# TODO: pass in 'csr' as a param, to ensure this is a csr matrix afterwards
-# validationWithPredictions = hstack([validationData, predictionsSparse])
-# printParent('validationData.shape' + str(validationData.shape))
-# printParent('len(predictions)' + str(len(predictions)) )
-
 # we must pass in 'csr', otherwise it will oftentimes pick another sparse matrix format for us that is not compatible with our sparse saver and loader function
 validationWithPredictions = hstack([validationData, predictions], 'csr')
-
-# printParent('validationWithPredictions.shape' + str(validationWithPredictions.shape))
 
 save_sparse_csr(outputFile, validationWithPredictions)
