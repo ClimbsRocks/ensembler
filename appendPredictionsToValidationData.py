e49f5dfeb52b333b11c445448b5ea30887b9c409
ClimbsRocks <climbsbytes@gmail.com>
2015-11-25 20:33:50 -0800

    finishes appending stage 0 predictions to validation data set, in sparse csr format!

--- appendPredictionsToValidationData.py
+++ appendPredictionsToValidationData.py
@@ -1,11 +1,14 @@
 import csv
 import sys
+import json
 
 import numpy as np
 from scipy.sparse import csr_matrix, hstack
 
-fileNames = sys.argv[2]
-# TODO: pass in these three arguments
+from sendMessages import printParent
+
+fileNames = json.loads(sys.argv[1])
+
 predictionsFile = fileNames['predictionsFile']
 validationData = fileNames['validationData']
 outputFile = fileNames['outputFile']
@@ -26,12 +29,25 @@ with open(predictionsFile, 'rU') as predictionsFile:
     for row in inputRows:
         predictions.append(row)
 
-load_sparse_csr(validationData)
+# predictions = np.ndarray(predictions)
+# predictions = predictions.astype(float)
+
+predictions = np.array(predictions, dtype=float)
+predictions = csr_matrix(predictions)
+
+validationData = load_sparse_csr(validationData)
 
 # put our predictions data into sparse format, so we can stack it with our validationData
-predictionsSparse = csr_matrix(predictions)
+# predictionsSparse = csr_matrix(np.array(predictions))
 
 # TODO: pass in 'csr' as a param, to ensure this is a csr matrix afterwards
-validationWithPredictions = hstack([validationData, predictionsSparse])
+# validationWithPredictions = hstack([validationData, predictionsSparse])
+# printParent('validationData.shape' + str(validationData.shape))
+# printParent('len(predictions)' + str(len(predictions)) )
+
+# we must pass in 'csr', otherwise it will oftentimes pick another sparse matrix format for us that is not compatible with our sparse saver and loader function
+validationWithPredictions = hstack([validationData, predictions], 'csr')
+
+# printParent('validationWithPredictions.shape' + str(validationWithPredictions.shape))
 
 save_sparse_csr(outputFile, validationWithPredictions)
